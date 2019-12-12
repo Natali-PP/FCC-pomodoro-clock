b63589b6e1cc45450c5831b89cbafa7b335ac75b
C Natalí Palacio Pastor <cristinanatalip@gmail.com>
2019-12-12 11:59:14 -0300

    Updates

---
+++ precache-manifest.f34fc871a125f6c284236924d0f4efef.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b58850451b01484f6b4e5166e95b47d",
+    "url": "natali-pp.github.io/FCC-pomodoro-clock/index.html"
+  },
+  {
+    "revision": "7631ca3e30061443e0bc",
+    "url": "natali-pp.github.io/FCC-pomodoro-clock/static/css/main.b3d275fd.chunk.css"
+  },
+  {
+    "revision": "e494459fde8ca015ae32",
+    "url": "natali-pp.github.io/FCC-pomodoro-clock/static/js/2.a0a4e420.chunk.js"
+  },
+  {
+    "revision": "7631ca3e30061443e0bc",
+    "url": "natali-pp.github.io/FCC-pomodoro-clock/static/js/main.1c253150.chunk.js"
+  },
+  {
+    "revision": "0f58fa5734ac00575f5d",
+    "url": "natali-pp.github.io/FCC-pomodoro-clock/static/js/runtime-main.4384d59a.js"
+  },
+  {
+    "revision": "20fab26e2be04787cb72178349fa874c",
+    "url": "natali-pp.github.io/FCC-pomodoro-clock/static/media/wronger-rave-hit.20fab26e.wav"
+  }
+]);
